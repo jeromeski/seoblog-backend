83261f65f13b6861b12196467fc9024fe84c1fd5
jeromeski <jeromegee@gmail.com>
2020-10-05 10:44:11 +0800

    Blogs search Backend

--- routes/blog.js
+++ routes/blog.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {
-  create,
-  list,
-  listAllBlogsCategoriesTags,
-  read,
-  remove,
-  update,
-  photo,
-  listRelated,
-  listSearch
+    create,
+    list,
+    listAllBlogsCategoriesTags,
+    read,
+    remove,
+    update,
+    photo,
+    listRelated,
+    listSearch
 } = require('../controllers/blog');
 
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
@@ -22,6 +22,6 @@ router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
 router.put('/blog/:slug', requireSignin, adminMiddleware, update);
 router.get('/blog/photo/:slug', photo);
 router.post('/blogs/related', listRelated);
-router.get('/blog/search', listSearch);
+router.get('/blogs/search', listSearch);
 
 module.exports = router;
